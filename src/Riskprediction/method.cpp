9eff407548e9e25bdb3e9dbab816cba533894233
y-kurio <you@example.comy.kurio0925@gmail.com>
2023-12-08 11:42:11 +0000

    noto

--- src/Riskprediction/method.cpp
+++ src/Riskprediction/method.cpp
@@ -1,5 +1,10 @@
 #include<mynteye_pointcloud/Riskprediction.h>
 
+
+
+
+
+
 void RiskClass::__Cluster_closest_pointcallback(const mynteye_pointcloud::pointDataConstPtr& msg)
 {
     Cluster_Minpts_pre_ = Cluster_Minpts_;
